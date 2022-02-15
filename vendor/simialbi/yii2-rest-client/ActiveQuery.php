34c89c800d4c376a1e24059f9f160c3b58cdae36
jonatas15 <jonatas.webdeveloper@gmail.com>
2022-02-15 18:34:18 -0300

    comp

---
+++ vendor/simialbi/yii2-rest-client/ActiveQuery.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simialbi
+ * Date: 12.10.2017
+ * Time: 10:34
+ */
+
+namespace simialbi\yii2\rest;
+
+use yii\base\InvalidConfigException;
+use yii\db\ActiveQueryInterface;
+use yii\db\ActiveQueryTrait;
+use yii\db\ActiveRelationTrait;
+
+/**
+ * Class RestQuery
+ */
+class ActiveQuery extends Query implements ActiveQueryInterface {
+	use ActiveQueryTrait;
+	use ActiveRelationTrait;
+
+	/**
+	 * @var array options for search
+	 */
+	public $options = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $modelClass the model class associated with this query
+	 * @param array $config configurations to be applied to the newly created query object
+	 */
+	public function __construct($modelClass, $config = []) {
+		$this->modelClass = $modelClass;
+		parent::__construct($config);
+	}
+
+
+	/**
+	 * Creates a DB command that can be used to execute this query.
+	 *
+	 * @param Connection $db the DB connection used to create the DB command.
+	 *                       If null, the DB connection returned by [[modelClass]] will be used.
+	 *
+	 * @return Command the created DB command instance.
+	 */
+	public function createCommand($db = null) {
+		/**
+		 * @var ActiveRecord $modelClass
+		 */
+		$modelClass = $this->modelClass;
+
+		if ($db === null) {
+			$db = $modelClass::getDb();
+		}
+
+		if ($this->from === null) {
+			$this->from($modelClass::modelName());
+		}
+
+//		if ($this->searchModel === null) {
+//			$this->searchModel = mb_substr(mb_strrchr($this->modelClass, '\\'), 1).'Search';
+//		}
+
+		return parent::createCommand($db);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function all($db = null) {
+		return parent::all($db);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function populate($rows) {
+		if (empty($rows)) {
+			return [];
+		}
+
+		$models = $this->createModels($rows);
+		if (!empty($this->join) && $this->indexBy === null) {
+			$models = $this->removeDuplicatedModels($models);
+		}
+		if (!empty($this->with)) {
+			$this->findWith($this->with, $models);
+		}
+		if (!$this->asArray) {
+			foreach ($models as $model) {
+				$model->afterFind();
+			}
+		}
+
+		return $models;
+	}
+
+	/**
+	 * Removes duplicated models by checking their primary key values.
+	 * This method is mainly called when a join query is performed, which may cause duplicated rows being returned.
+	 *
+	 * @param array $models the models to be checked
+	 *
+	 * @throws InvalidConfigException if model primary key is empty
+	 * @return array the distinctive models
+	 */
+	private function removeDuplicatedModels($models) {
+		$hash = [];
+		/* @var $class ActiveRecord */
+		$class = $this->modelClass;
+		$pks   = $class::primaryKey();
+
+		if (count($pks) > 1) {
+			// composite primary key
+			foreach ($models as $i => $model) {
+				$key = [];
+				foreach ($pks as $pk) {
+					if (!isset($model[$pk])) {
+						// do not continue if the primary key is not part of the result set
+						break 2;
+					}
+					$key[] = $model[$pk];
+				}
+				$key = serialize($key);
+				if (isset($hash[$key])) {
+					unset($models[$i]);
+				} else {
+					$hash[$key] = true;
+				}
+			}
+		} elseif (empty($pks)) {
+			throw new InvalidConfigException("Primary key of '{$class}' can not be empty.");
+		} else {
+			// single column primary key
+			$pk = reset($pks);
+			foreach ($models as $i => $model) {
+				if (!isset($model[$pk])) {
+					// do not continue if the primary key is not part of the result set
+					break;
+				}
+				$key = $model[$pk];
+				if (isset($hash[$key])) {
+					unset($models[$i]);
+				} elseif ($key !== null) {
+					$hash[$key] = true;
+				}
+			}
+		}
+
+		return array_values($models);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function one($db = null) {
+		$row = parent::one($db);
+		if ($row !== false) {
+			$models = $this->populate(isset($row[0]) ? $row : [$row]);
+
+			return reset($models) ?: null;
+		}
+
+		return null;
+	}
+}
---
+++ vendor/simialbi/yii2-rest-client/README.md
@@ -0,0 +1,74 @@
+# REST Client for Yii 2 (ActiveRecord-like model)
+This extension provides an interface to work with RESTful API via ActiveRecord-like model in Yii 2.
+It is based on [ApexWire's](https://github.com/ApexWire) [yii2-restclient](https://github.com/ApexWire/yii2-restclient).
+
+## Resources
+ * [yii2-restclient](https://github.com/ApexWire/yii2-restclient)
+ * [Yii2 HiArt](https://github.com/hiqdev/yii2-hiart).
+ * [\yii\db\ActiveRecord](http://www.yiiframework.com/doc-2.0/guide-db-active-record.html)
+
+## Installation
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+$ php composer.phar require --prefer-dist simialbi/yii2-rest-client
+```
+
+or add
+
+```
+"simialbi/yii2-rest-client": "*"
+```
+
+to the `require` section of your `composer.json`.
+
+## Configuration
+To use this extension, configure restclient component in your application config:
+
+```php
+    'components' => [
+        'rest' => [
+            'class'   => 'simialbi\yii2\rest\Connection',
+            'baseUrl' => 'https://api.site.com/'
+        ],
+    ],
+```
+
+## Usage
+Define your Model
+
+```php
+<?php
+
+namespace app\models;
+
+use simialbi\yii2\rest\ActiveRecord;
+
+class MyModel extends ActiveRecord {
+	/**
+	 * @inheritdoc
+	 */
+	public static function modelName() {
+		return 'my-super-model-name';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public static function primaryKey() {
+		return ['id'];
+	}
+}
+```
+
+The usage after define a model is the same like [yii\db\ActiveRecord](http://www.yiiframework.com/doc-2.0/guide-db-active-record.html)
+
+## License
+
+**yii2-rest-client** is released under MIT license. See bundled [LICENSE](LICENSE) for details.
+
+## Acknowledgments
+ * [ApexWire's](https://github.com/ApexWire) [yii2-restclient](https://github.com/ApexWire/yii2-restclient)
+ * [Yii2 HiArt](https://github.com/hiqdev/yii2-hiart).
